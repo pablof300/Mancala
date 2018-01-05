835395211c4df15e90d9938b6808becc9973c63c
pablof300 <pablof300@gmail.com>
2018-01-05 18:21:56 -0500

    Testing marble animation and fixing small UI glithces

--- src/main/java/me/pabloestrada/Controllers/GameplayController.java
+++ src/main/java/me/pabloestrada/Controllers/GameplayController.java
@@ -13,33 +13,43 @@ public class GameplayController {
 
 	@FXML
 	private ImageView slot;
-	
+
 	@FXML
 	private StackPane marbleHolder;
-	
+
 	@FXML
 	private void initialize() {
-		TranslateTransition tt = new TranslateTransition(Duration.millis(2000), slot);
-	     //tt.setByX(200f);
-	     tt.setToX(-305);
-	     tt.setToY(136);
-	     tt.setCycleCount(1);
-	     tt.setAutoReverse(true);
-	     tt.play();
-	     
-	     Timer t = new Timer();
-	     t.schedule(new TimerTask() {
+		/*TranslateTransition tt = new TranslateTransition(Duration.millis(2000), slot);
+		// tt.setByX(200f);
+		tt.setToX(-305);
+		tt.setToY(136);
+		tt.setCycleCount(1);
+		tt.setAutoReverse(true);
+		tt.play();*/
 
-			@Override
-			public void run() {
-				TranslateTransition tt = new TranslateTransition(Duration.millis(2000), slot);
-			     //tt.setByX(200f);
-			     tt.setToX(-68);
-			 
-			     tt.setCycleCount(1);
-			     tt.setAutoReverse(true);
-			     tt.play();
-			}}, 5000);
+		Timer t = new Timer();
+
+		final int[] yPos = { 136, 136, 136, 136, 136, 136, 223, 330, 330, 330, 330, 330, 330, 242 };
+		final int[] xPos = { 300, 182, 62, -64, -186, -305, -425, -305, -186, -64, 62, 182, 300, 420 };
+
+		int time = 0;
+		for (int i = 0; i < xPos.length; i++) {
+			final int counter = i;
+			t.schedule(new TimerTask() {
+
+				@Override
+				public void run() {
+					TranslateTransition tt = new TranslateTransition(Duration.millis(1000), slot);
+					// tt.setByX(200f);
+					tt.setToX(xPos[counter]);
+					tt.setToY(yPos[counter]);
+
+					tt.setCycleCount(1);
+					tt.setAutoReverse(true);
+					tt.play();
+				}
+			}, time++ * 1000);
+		}
 	}
-	
+
 }
--- src/main/java/me/pabloestrada/MancalaGame/board/Board.java
+++ src/main/java/me/pabloestrada/MancalaGame/board/Board.java
@@ -5,9 +5,10 @@ import me.pabloestrada.MancalaGame.slots.Slot;
 public class Board {
 
 	private Slot[] slots;
-	
+
 	public Board() {
-		//int[] yPos = 
+		int[] yPos = { 136, 136, 136, 136, 136, 136, 223, 330, 330, 330, 330, 330, 330, 242 };
+		int[] xPos = { 300, 182, 62, -64, -186, -305, -425, -305, -186, -64, 62, 182, 300, 420 };
 	}
-	
+
 }
