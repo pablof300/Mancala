96e2a196504873a7e85f8cdeac13cd1e62337f9d
pablof300 <pablof300@gmail.com>
2018-01-05 10:28:02 -0500

    Started working on the UI of the gameplay and added more art

--- src/main/java/me/pabloestrada/Controllers/MainMenuController.java
+++ src/main/java/me/pabloestrada/Controllers/MainMenuController.java
@@ -1,5 +1,50 @@
 package me.pabloestrada.Controllers;
 
+import java.awt.Desktop;
+import java.net.MalformedURLException;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.URL;
+
+import javafx.fxml.FXML;
+
 public class MainMenuController {
 
+	@FXML
+	private void launchStart() {
+		System.out.println("Start");
+	}
+	
+	@FXML
+	private void launchGithub() throws MalformedURLException {
+		openWebpage(new URL("https://github.com/pablof300/Mancala"));
+	}
+	
+	@FXML
+	private void launchHelp() {
+		System.out.println("Help");
+	}
+	
+	private boolean openWebpage(URI uri) {
+	    Desktop desktop = Desktop.isDesktopSupported() ? Desktop.getDesktop() : null;
+	    if (desktop != null && desktop.isSupported(Desktop.Action.BROWSE)) {
+	        try {
+	            desktop.browse(uri);
+	            return true;
+	        } catch (Exception e) {
+	            e.printStackTrace();
+	        }
+	    }
+	    return false;
+	}
+
+	private boolean openWebpage(URL url) {
+	    try {
+	        return openWebpage(url.toURI());
+	    } catch (URISyntaxException e) {
+	        e.printStackTrace();
+	    }
+	    return false;
+	}
+	
 }
